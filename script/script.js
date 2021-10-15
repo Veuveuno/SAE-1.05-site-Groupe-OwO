419553a4ce1b0939ba0773f251da3008c76a8ecc
MariusPisto <91264105+MariusPisto@users.noreply.github.com>
2021-10-15 16:24:47 +0200

    petite boulette

--- script/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
